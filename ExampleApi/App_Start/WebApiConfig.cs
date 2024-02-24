c73d208872f97ab249ea13238988c630309ac094
TaylorSumers <suvorint@gmail.com>
2024-02-24 21:34:19 +0300

    comm3

--- ExampleApi/App_Start/WebApiConfig.cs
+++ ExampleApi/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web.Http;
 
 namespace Example
@@ -19,6 +20,9 @@ namespace Example
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Formatters.Clear();
+            config.Formatters.Add(new JsonMediaTypeFormatter());
         }
     }
 }
