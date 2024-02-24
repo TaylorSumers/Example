5776765aafe01f456d137603a45fe31ed0a62571
TaylorSumers <suvorint@gmail.com>
2024-02-24 19:16:26 +0300

    Add project files.

---
+++ ExampleApi/Controllers/RolesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ExampleApi.Controllers
+{
+    public class RolesController : ApiController
+    {
+        public IHttpActionResult GetRoles()
+        {
+
+        }
+    }
+}
---
+++ ExampleApi/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ExampleApi.Controllers
+{
+    public class UsersController : ApiController
+    {
+        public IHttpActionResult GetUsers()
+        {
+
+        }
+        
+        public IHttpActionResult PutUser()
+        {
+
+        }
+
+        public IHttpActionResult PostUser()
+        {
+
+        }
+    }
+}
